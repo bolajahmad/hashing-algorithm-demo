1b5f618a8500c997284ae939ab615a9e9e28d0fc
Bolaji Ahmad <56865496+bolajahmad@users.noreply.github.com>
2024-07-08 13:11:35 +0100

    fixed all typos and referenced appropriate variables

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,65 @@ use std::convert::TryInto;
 use std::str;
 use rug::{float, Float};
 
+#[inline(always)]
+fn rotate_right(x: u64, n: u32) -> u64 {
+    (x >> n) | (x << (64 - n))
+}
+
+#[inline(always)]
+fn ms_sigma0(x: u64) -> u64 {
+    rotate_right(x, 1) ^ rotate_right(x, 8) ^ (x >> 7)
+}
+
+#[inline(always)]
+fn ms_sigma1(x: u64) -> u64 {
+    rotate_right(x, 19) ^ rotate_right(x, 61) ^ (x >> 6)
+}
+
+#[inline(always)]
+fn main_sigma0(x: u64) -> u64 {
+    rotate_right(x, 28) ^ rotate_right(x, 34) ^ rotate_right(x, 39)
+}
+
+#[inline(always)]
+fn main_sigma1(x: u64) -> u64 {
+    rotate_right(x, 14) ^ rotate_right(x, 18) + rotate_right(x, 41)
+}
+
+#[inline(always)]
+fn choice(x: u64, y: u64, z: u64) -> u64 {
+    (x & y) ^ (!x & z)
+}
+
+#[inline(always)]
+fn majority(x: u64, y: u64, z: u64) -> u64 {
+    (x & y) ^ (x & z) ^ (y & z)
+}
+
 // These 80 constants must be defined for SHA512 algorithm
 // The words are used each in the 80 rounds of the algorithm
 // logically, they are derived from the fractional part of the cube root of the first 80 prime numbers
 const SHA_CONSTANTS: [u64; 80] = [
-    "0x428a2f98d728ae22", "0x7137449123ef65cd", "0xb5c0fbcfec4d3b2f", "0xe9b5dba58189dbbc",
-    "0x3956c25bf348b538", "0x59f111f1b605d019", "0x923f82a4af194f9b", "0xab1c5ed5da6d8118",
-    "0xd807aa98a3030242", "0x12835b0145706fbe", "0x243185be4ee4b28c", "0x550c7dc3d5ffb4e2",
-    "0x72be5d74f27b896f", "0x80deb1fe3b1696b1", "0x9bdc06a725c71235", "0xc19bf174cf692694",
-    "0xe49b69c19ef14ad2", "0xefbe4786384f25e3", "0x0fc19dc68b8cd5b5", "0x240ca1cc77ac9c65",
-    "0x2de92c6f592b0275", "0x4a7484aa6ea6e483", "0x5cb0a9dcbd41fbd4", "0x76f988da831153b5",
-    "0x983e5152ee66dfab", "0xa831c66d2db43210", "0xb00327c898fb213f", "0xbf597fc7beef0ee4",
-    "0xc6e00bf33da88fc2", "0xd5a79147930aa725", "0x06ca6351e003826f", "0x142929670a0e6e70",
-    "0x27b70a8546d22ffc", "0x2e1b21385c26c926", "0x4d2c6dfc5ac42aed", "0x53380d139d95b3df",
-    "0x650a73548baf63de", "0x766a0abb3c77b2a8", "0x81c2c92e47edaee6", "0x92722c851482353b",
-    "0xa2bfe8a14cf10364", "0xa81a664bbc423001", "0xc24b8b70d0f89791", "0xc76c51a30654be30",
-    "0xd192e819d6ef5218", "0xd69906245565a910", "0xf40e35855771202a", "0x106aa07032bbd1b8",
-    "0x19a4c116b8d2d0c8", "0x1e376c085141ab53", "0x2748774cdf8eeb99", "0x34b0bcb5e19b48a8",
-    "0x391c0cb3c5c95a63", "0x4ed8aa4ae3418acb", "0x5b9cca4f7763e373", "0x682e6ff3d6b2b8a3",
-    "0x748f82ee5defb2fc", "0x78a5636f43172f60", "0x84c87814a1f0ab72", "0x8cc702081a6439ec",
-    "0x90befffa23631e28", "0xa4506cebde82bde9", "0xbef9a3f7b2c67915", "0xc67178f2e372532b",
-    "0xca273eceea26619c", "0xd186b8c721c0c207", "0xeada7dd6cde0eb1e", "0xf57d4f7fee6ed178",
-    "0x06f067aa72176fba", "0x0a637dc5a2c898a6", "0x113f9804bef90dae", "0x1b710b35131c471b",
-    "0x28db77f523047d84", "0x32caab7b40c72493", "0x3c9ebe0a15c9bebc", "0x431d67c49c100d4c",
-    "0x4cc5d4becb3e42b6", "0x597f299cfc657e2a", "0x5fcb6fab3ad6faec", "0x6c44198c4a475817",
+    0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc,
+    0x3956c25bf348b538, 0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118,
+    0xd807aa98a3030242, 0x12835b0145706fbe, 0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2,
+    0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235, 0xc19bf174cf692694,
+    0xe49b69c19ef14ad2, 0xefbe4786384f25e3, 0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65,
+    0x2de92c6f592b0275, 0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5,
+    0x983e5152ee66dfab, 0xa831c66d2db43210, 0xb00327c898fb213f, 0xbf597fc7beef0ee4,
+    0xc6e00bf33da88fc2, 0xd5a79147930aa725, 0x06ca6351e003826f, 0x142929670a0e6e70,
+    0x27b70a8546d22ffc, 0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed, 0x53380d139d95b3df,
+    0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6, 0x92722c851482353b,
+    0xa2bfe8a14cf10364, 0xa81a664bbc423001, 0xc24b8b70d0f89791, 0xc76c51a30654be30,
+    0xd192e819d6ef5218, 0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8,
+    0x19a4c116b8d2d0c8, 0x1e376c085141ab53, 0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8,
+    0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb, 0x5b9cca4f7763e373, 0x682e6ff3d6b2b8a3,
+    0x748f82ee5defb2fc, 0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec,
+    0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915, 0xc67178f2e372532b,
+    0xca273eceea26619c, 0xd186b8c721c0c207, 0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178,
+    0x06f067aa72176fba, 0x0a637dc5a2c898a6, 0x113f9804bef90dae, 0x1b710b35131c471b,
+    0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc, 0x431d67c49c100d4c,
+    0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817,
 ];
 
 const BLOCK_SIZE: usize = 16; // Size of each block in bits
@@ -80,7 +115,7 @@ impl SHA512Hasher {
 
         // prepare the message schedule
         for t in 0..16 {
-            w[t] = u64::from_be_bytes([
+            words[t] = u64::from_be_bytes([
                 block[t * 8 + 0], block[t * 8 + 1], block[t * 8 + 2],
                 block[t * 8 + 3], block[t * 8 + 4], block[t * 8 + 5],
                 block[t * 8 + 6], block[t * 8 + 7]
@@ -88,10 +123,10 @@ impl SHA512Hasher {
         }
 
         for t in 16..80 {
-            w[t] = ms_sigma1(w[t - 2])
-                .wrapping_add(w[t - 7])
-                .wrapping_add(ws_sigma0(w[t - 15]))
-                .wrapping_add(w[t - 16]);
+            words[t] = ms_sigma1(words[t - 2])
+                .wrapping_add(words[t - 7])
+                .wrapping_add(ms_sigma0(words[t - 15]))
+                .wrapping_add(words[t - 16]);
         }
 
         // define the working variable
@@ -106,23 +141,23 @@ impl SHA512Hasher {
 
         
         for t in 0..80 {
-            let t1 = h
-                .wrapping_add(choice(e, f, g))
-                .wrapping_add(main_sigma1(e))
-                .wrapping_add(w[t])
+            let t1 = (h as u64)
+                .wrapping_add(choice(e.into(), f.into(), g.into()))
+                .wrapping_add(main_sigma1(e.into()))
+                .wrapping_add(words[t])
                 .wrapping_add(SHA_CONSTANTS[t]);
 
-            let t2 = main_sigma0(a)
-                .wrapping_add(majority(a, b, c));
+            let t2 = main_sigma0(a.into())
+                .wrapping_add(majority(a.into(), b.into(), c.into()));
 
             h = g;
             g = f;
             f = e;
-            e = d.wrapping_add(t1);
+            e = d.wrapping_add(t1.try_into().unwrap());
             d = c;
             c = b;
             b = a;
-            a = t1.wrapping_add(t2);
+            a = t1.wrapping_add(t2).try_into().unwrap();
         }
 
         // Update the state
@@ -160,41 +195,6 @@ impl SHA512Hasher {
         message.extend_from_slice(&length_bytes);
         message
     }
-}
-
-#[inline(always)]
-fn rotate_right(x: u64, n: u32) -> u64 {
-    (x >> n) | (x << (64 - n))
-}
-
-#[inline(always)]
-fn ms_sigma0(x: u64) -> u64 {
-    rotate_right(x, 1) ^ rotate_right(x, 8) ^ (x >> 7)
-}
-
-#[inline(always)]
-fn ms_sigma1(x: u64) -> u64 {
-    rotate_right(x, 19) ^ rotate_right(x, 61) ^ (x >> 6)
-}
-
-#[inline(always)]
-fn main_sigma0(x: u64) -> u64 {
-    rotate_right(x, 28) ^ rotate_right(x, 34) ^ rotate_right(x, 39)
-}
-
-#[inline(always)]
-fn main_sigma1(x: u64) -> u64 {
-    rotate_right(x, 14) ^ rotate_right(x, 18) + rotate_right(x, 41)
-}
-
-#[inline(always)]
-fn choice(x: u64, y: u64, z: u64) -> u64 {
-    (x & y) ^ (!x & z)
-}
-
-#[inline(always)]
-fn majority(x: u64, y: u64, z: u64) -> u64 {
-    (x & y) ^ (x & z) ^ (y & z)
 }
 
 fn convert_f64_to_bytes(value: &f64) -> u64 {
